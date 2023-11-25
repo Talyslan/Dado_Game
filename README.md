fb21f859c5e16d6416bf7ad1cf5b8add4f9b7046
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-11-25 01:51:04 -0300

    update: all project

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center"> Dicee Game 🎲</h1>
+
+<img src="https://github.com/Talyslan/Gerador-de-Cancelamento/assets/78499700/79dc42a1-54ff-421d-be7a-a9fb8cabc11b" alt="Layout no Computador" />
+<img src="https://github.com/Talyslan/Gerador-de-Cancelamento/assets/78499700/c56c83f7-6098-43a5-a9b3-26fd7c7ebfc4" alt="Layout no Celular" />
+
+
+## 🗿 O Projeto
+
+Dicee Game é um jogo baseado no sorteio de dois dados, o dado que tirar o maior número vence e se números iguais forem sorteados, empate é declarado. Com o intuito de continuar meus estudos de DOM este projeto foi criado, ideia inicialmente trazida das aulas da Blessed e inspiração do instagram.
+
+## 📌 Como ver
+
+O projeto está hospedado no próprio GithubPages:
+[Gerador de Cancelamento](https://talyslan.github.io/Dicee-Game/)
+
+
+## Technologies Used
+
+A criação deste site utiliza as tecnologias básicas para uma página web:
+
+- HTML
+- CSS
+- JavaScript
+
+Feito por <a href="https://github.com/Talyslan">Talyslan Canabarro</a>
+<br/>
+<img src="https://img.shields.io/badge/license-MIT-8A2BE2" alt="license mit" />
--- script.js
+++ script.js
@@ -1,41 +1,14 @@
 const img1 = document.getElementsByTagName("img")[0];
-const img2 = document.getElementsByTagName("img")[1];
 const box1 = document.getElementsByClassName("box")[0];
+const img2 = document.getElementsByTagName("img")[1];
 const box2 = document.getElementsByClassName("box")[1];
-const result = document.getElementsByTagName("span")[2];
 
-const imagens = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
+const points1_HTML = document.getElementById("points1");
+const points2_HTML = document.getElementById("points2");
 
-function jogar() {
-  removeClass();
-  //sorteio
-  let sortear_img1 = Math.floor(Math.random() * 5 + 1);
-  let sortear_img2 = Math.floor(Math.random() * 5 + 1);
-
-  img1.src = `./imagens/dice${sortear_img1}.png`;
-  img2.src = `./imagens/dice${sortear_img2}.png`;
-
-  //verificação do resultado
-  if (sortear_img1 > sortear_img2) {
-    result.innerHTML = `Player 1 is winner! 👑`;
-    box1.classList.add('winner');
-  }
-  else if (sortear_img2 > sortear_img1) {
-    result.innerHTML = `Player 2 is winner! 👑`;
-    box2.classList.add('winner');
-  }
-  else if (sortear_img1 == sortear_img2) {
-    result.innerHTML = `Draw!😯`;
-    box1.classList.add('draw');
-    box2.classList.add('draw');
-  }
-}
-
-function reset() {
-  title.innerHTML = `Empate!😯`;
-  img1.src = `./imagens/${img_escolhida_1}`;
-  img2.src = `./imagens/${img_escolhida_1}`;
-}
+const result = document.getElementById("putAnswer");
+const btnPlay = document.getElementById("rollDices");
+const btnReset = document.getElementById("reset");
 
 function removeClass() {
   box1.classList.remove('winner');
@@ -43,3 +16,52 @@ function removeClass() {
   box2.classList.remove('winner');
   box2.classList.remove('draw');
 }
+
+let points1 = 0;
+let points2 = 0;
+
+function play() {
+  removeClass();
+  //sorteio
+  const player1 = Math.floor(Math.random() * 5 + 1);
+  const player2 = Math.floor(Math.random() * 5 + 1);
+
+  img1.src = `./imagens/dado/dice${player1}.png`;
+  img2.src = `./imagens/dado/dice${player2}.png`;
+
+  //verificação do resultado
+  if (player1 > player2) {
+    result.innerHTML = `Player 1 is winner! 👑`;
+    box1.classList.add('winner');
+    points1++;
+    points1_HTML.innerHTML = points1;
+  }
+  else if (player2 > player1) {
+    result.innerHTML = `Player 2 is winner! 👑`;
+    box2.classList.add('winner');
+    points2++;
+    points2_HTML.innerHTML = points2;
+  }
+  else if (player1 == player2) {
+    result.innerHTML = `Draw!😯`;
+    box1.classList.add('draw');
+    box2.classList.add('draw');
+  }
+}
+
+function reset() {
+  removeClass();
+  const urlImg1 = "./imagens/dado/dice1.png"
+  result.innerHTML = `Click on Button to Play`;
+  img1.src = urlImg1;
+  img2.src = urlImg1;
+
+  points1 = 0;
+  points2 = 0;
+
+  points1_HTML.innerHTML = points1;
+  points2_HTML.innerHTML = points2;
+}
+
+btnPlay.addEventListener("click", play);
+btnReset.addEventListener("click", reset);
